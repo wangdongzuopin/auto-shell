f8d488efffa47fdd097583a0f3e1fa4de53b0fa3
wangdong <wangdong@agilestar.cn>
2026-03-27 11:32:07 +0800

    chore: add project configuration files

---
+++ electron.vite.config.ts
@@ -0,0 +1,31 @@
+import { defineConfig } from 'electron-vite';
+import { resolve } from 'path';
+
+export default defineConfig({
+  main: {
+    build: {
+      rollupOptions: {
+        external: ['keytar', 'node-pty']
+      }
+    }
+  },
+  preload: {
+    build: {
+      rollupOptions: {
+        input: {
+          index: resolve(__dirname, 'src/preload.ts')
+        }
+      }
+    }
+  },
+  renderer: {
+    root: 'src/renderer',
+    build: {
+      rollupOptions: {
+        input: {
+          index: resolve(__dirname, 'src/renderer/index.html')
+        }
+      }
+    }
+  }
+});
